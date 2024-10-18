b6501fee836d1d5aa15a29ded33fff554b9bf96f
dbenfon341 <dbenfon341@g.educaand.es>
2024-10-18 16:33:17 +0200

    2_bucles terminado

--- src/bucles/ej22_02.py
+++ src/bucles/ej22_02.py
@@ -1,8 +1,32 @@
 # Ejercicio 2.2.2
 # Escribir un programa que pregunte al usuario su edad y muestre por pantalla todos los años que ha cumplido (desde 1 hasta su edad).
+from utils import *
+
+def validar_edad(edad: int) -> bool:
+    if edad < 0 or edad > 100:
+        return False
+    return True
+
+
+def anios_cumplidos(edad: int) -> str:
+    serie_edad = ""
+    for i in range(edad+1):
+        serie_edad = f"{(serie_edad)} + {i}"
+    return serie_edad[3:]
+
+def mostrar_serie_edad(edad) -> str:
+    serie_edad = anios_cumplidos(edad)
+    return f"Para llegar a tu edad actual ({edad}) has tenido que cumplir {serie_edad} años"
+    
 
 def main():
-    return
+    edad = pedir_num("Introduce tu edad: ")
+
+    while validar_edad(edad) is False:
+        edad = pedir_num("Introduce una edad correcta: ")
+
+    print(mostrar_serie_edad(edad))
+
 
 if __name__ == "__main__":
     main()
