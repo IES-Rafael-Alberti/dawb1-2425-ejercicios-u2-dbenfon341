394936d7eeca471dfbc4661f5d35ff2e826f60d0
dbenfon341 <dbenfon341@g.educaand.es>
2024-10-18 19:00:43 +0200

    11 y 12 bucles

--- src/bucles/ej22_11.py
+++ src/bucles/ej22_11.py
@@ -2,7 +2,14 @@
 # Escribir un programa que pida al usuario una palabra y luego muestre por pantalla una a una las letras de la palabra introducida empezando por la última.
 
 def main():
-    return
+
+    palabra = "ejercicio"
+
+    for letras in reversed(palabra):
+        print (letras)
+
+    for letras in palabra[::-1]:
+        print (letras)
 
 if __name__ == "__main__":
     main()
--- src/bucles/ej22_12.py
+++ src/bucles/ej22_12.py
@@ -2,7 +2,15 @@
 # Escribir un programa en el que se pregunte al usuario por una frase y una letra, y muestre por pantalla el número de veces que aparece la letra en la frase.
 
 def main():
-    return
+
+    palabra = "ejercicio"
+    
+    cont = 0
+    for letras in palabra:
+        cont += 1
+    
+    print (cont)
+
 
 if __name__ == "__main__":
     main()
