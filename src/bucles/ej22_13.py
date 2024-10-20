3ce825b772a07b1bfa38adfeac243df48b406b1b
dbenfon341 <dbenfon341@g.educaand.es>
2024-10-20 19:09:27 +0200

    Telepizza for you.

--- src/bucles/ej22_13.py
+++ src/bucles/ej22_13.py
@@ -1,5 +1,12 @@
 # Ejercicio 2.2.13
 # Escribir un programa que muestre el eco de todo lo que el usuario introduzca hasta que el usuario escriba “salir” que terminará.
+frase = ""
+palabra = ""
+while palabra != "fin":
+    palabra = input("Introduce una palabra: ")
+    frase += (f" {palabra}")
+    print(frase.strip())
+
 
 def main():
     return
--- src/condicionales/ej21_10.py
+++ src/condicionales/ej21_10.py
@@ -13,13 +13,15 @@ from utils import *
 # Listas con ingredientes
 ingredientes_vegetarianos = ["Pimiento", "Tofu"]
 ingredientes_no_vegetarianos = ["Peperoni", "Jamón", "Salmón"]
+ingredientes_default =  ["Tomate", "Mozzarella"]
 
-
-INGREDIENTES_DEFAULT =  "Tomate", "Mozzarella"
 OPCIONES_VEG = "vegetariana", "veg"
 OPCIONES_NO_VEG = "no vegetariana", "no veg"
 
-def preguntar_tipo_pizza(respuesta: int) -> bool:
+def preguntar_tipo_pizza(respuesta: str) -> bool:
+    """
+    
+    """
     # True vegetariano
     # False No vegetariano
     
@@ -27,54 +29,91 @@ def preguntar_tipo_pizza(respuesta: int) -> bool:
         return True
     elif respuesta in OPCIONES_NO_VEG:
         return False
-    else:
-        return None
 
-def escoger_ingrediente(opcion) -> tuple:
+def mostrar_ingredientes(tipo_de_pizza: bool) -> str:
+    """
     
-
-    return
-
-def mostrar_pizza(opcion_ingrediente) -> str:
-    return
-
-def mostrar_ingredientes_veg():
+    """
     print(f"Puedes elegir SOLO UNO de los siguientes ingredientes para tu pizza: ")
-    for _ in ingredientes_vegetarianos:
-        print(_)
+    if tipo_de_pizza is True:
+        for _ in ingredientes_vegetarianos:
+            print(_)
+    elif tipo_de_pizza is False:
+        for _ in ingredientes_no_vegetarianos:
+            print(_)
+
+def escoger_ingrediente(opcion: str, tipo_pizza: bool) -> list:
+    """
+    
+    """
+
+    ingredientes_pizza = list(ingredientes_default)
+
+    if tipo_pizza:
+        if opcion in ingredientes_vegetarianos:
+            ingredientes_pizza.append(opcion)
+            return ingredientes_pizza
+    else:
+        if opcion in ingredientes_no_vegetarianos:
+            ingredientes_pizza.append(opcion)
+            return ingredientes_pizza
+
+    return ingredientes_default
+
+def mostrar_pizza(tipo_pizza, ingredientes_pizza) -> str:
+    """
+    
+    """
+    if tipo_pizza is True:
+        tipo_pizza = "vegetariana"
+    else:
+        tipo_pizza = "no vegetariana"
+
+    return f"Tu pizza {tipo_pizza} lleva {', '.join(ingredientes_pizza)}."
 
 def menu():
+    """
+    
+    """
     print("Bienvenido a la pizzeria Bella Napoli")
     print("Tenemos dos tipos de pizza: Vegetariana y No vegetariana")
-    print(f"TODAS las pizzas de nuestra carta llevan {INGREDIENTES_DEFAULT[0]} y {INGREDIENTES_DEFAULT[1]}.")
+    print(f"TODAS las pizzas de nuestra carta llevan {ingredientes_default[0]} y {ingredientes_default[1]}.")
     print(f"Las pizzas veganas pueden llevar unos ingredientes u otros")
     print(f"Solo puedes escoger un ingrediente extra para cada tipo de pizza.")
 
 
 def main():
+    """
+    
+    """
     clear()
     menu()
-    validar_pizza = None
-    while validar_pizza is None:
+    tipo_de_pizza = None
+    while tipo_de_pizza is None:
         tipo_de_pizza = input("Introduce el tipo de pizza que deseas: ").strip().lower()
         if preguntar_tipo_pizza(tipo_de_pizza) is True:
-            validar_pizza = 1
+            tipo_de_pizza = preguntar_tipo_pizza(tipo_de_pizza)
             clear()
-            print("Has escogido una pizza vegetariana")
-            mostrar_ingredientes_veg()
+            print("Has escogido una pizza vegetariana.")
+            mostrar_ingredientes(tipo_de_pizza)
+
+            opcion = input("¿Qué ingrediente deseas añadir a tu pizza?: ").strip().capitalize()
+            ingredientes = escoger_ingrediente(opcion, tipo_de_pizza)
+            print(mostrar_pizza(tipo_de_pizza, ingredientes))
+
         elif preguntar_tipo_pizza(tipo_de_pizza) is False:
+            tipo_de_pizza = preguntar_tipo_pizza(tipo_de_pizza)
             clear()
-            print("Has escogido una pizza no vegetariana")
-            mostrar_ingredientes_veg()
+            print("Has escogido una pizza no vegetariana.")
+            mostrar_ingredientes(tipo_de_pizza)
+
+            opcion = input("¿Qué ingrediente deseas añadir a tu pizza?: ").strip().capitalize()
+            ingredientes = escoger_ingrediente(opcion, tipo_de_pizza)
+            print(mostrar_pizza(tipo_de_pizza, ingredientes))
+
         else:
             print("Esa pizza no existe.")
-            
-    
-
-
-
-    # print(INGREDIENTES_DEFAULT[1])
-    return
+            tipo_de_pizza = None
 
 if __name__ == "__main__":
     main()
