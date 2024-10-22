671ff21225243fb0778a941fd686e58ffd3ede6e
dbenfon341 <dbenfon341@g.educaand.es>
2024-10-22 03:30:03 +0200

    Algoritmo burbuja + comentario.

--- src/bucles/ej22_16.py
+++ src/bucles/ej22_16.py
@@ -1,8 +1,6 @@
 # Ejercicio 2.2.16
 # Leer números enteros positivos de teclado, hasta que el usuario ingrese el 0. Informar cuál fue el mayor número ingresado.
 
-
-
 def main():
     while True:
         numero = int(input("Introduce un número: "))
